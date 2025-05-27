f8a2d8e73ff72c8698fc64e983f8574f343b19c3
Cegem-360 <144362694+Cegem-360@users.noreply.github.com>
2025-05-27 19:56:17 +0200

    homepage build

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,34 +1,49 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-    <head>
-        <meta charset="utf-8" />
+<head>
+    <meta charset="utf-8" />
 
-        <meta name="application-name" content="{{ config('app.name') }}" />
-        <meta name="csrf-token" content="{{ csrf_token() }}" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="application-name" content="{{ config('app.name') }}" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <title>{{ config('app.name') }}</title>
+    @isset($title)
+        <title>{{ $title }}</title>
+    @else
+        <title>{{ __('page.title.default') }}</title>
+    @endisset
+    @isset($meta)
+        {{ $meta }}
+    @else
+        <meta name="robots" content="index, follow">
+        <meta name="googlebot" content="index, follow">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <link rel="canonical" href="{{ Request::url() }}">
+    @endisset
 
-        <style>
-            [x-cloak] {
-                display: none !important;
-            }
-        </style>
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
-        @filamentStyles
-        @vite('resources/js/app.js')
+    <style>
+        [x-cloak] {
+            display: none !important;
+        }
+    </style>
 
-    </head>
+    @filamentStyles
+    @vite('resources/js/app.js')
 
-    <body class="antialiased">
+</head>
 
-        {{ $slot }}
+<body class="antialiased">
 
-        @livewire('notifications')
-        
-        @filamentScripts
-        
-    </body>
+    {{ $slot }}
+
+    @livewire('notifications')
+
+    @filamentScripts
+
+</body>
 
 </html>
