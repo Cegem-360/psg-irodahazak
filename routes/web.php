515f3b8c565129a80765716c609aeffc0c7e0bd3
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2025-06-13 15:24:57 +0200

    fix: remove trailing slashes from route definitions

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'index')->name('home');
-Route::view('/adatlap-oldal/', 'index')->name('adatlap-oldal');
-Route::view('/kiado-irodak/', 'index')->name('kiado-irodak');
-Route::view('/hirek/', 'index')->name('hirek');
-Route::view('/rolunk/', 'index')->name('rolunk');
-Route::view('/kapcsolat/', 'index')->name('kapcsolat');
+Route::view('/adatlap-oldal', 'index')->name('adatlap-oldal');
+Route::view('/kiado-irodak', 'index')->name('kiado-irodak');
+Route::view('/hirek', 'index')->name('hirek');
+Route::view('/rolunk', 'index')->name('rolunk');
+Route::view('/kapcsolat', 'index')->name('kapcsolat');
 
 /* Route::get('/', function () {
     return view('components.layouts.home');
