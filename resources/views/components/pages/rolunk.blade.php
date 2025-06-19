07bd5b13e699679bbd9dc3ee57954af75273b63b
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2025-06-19 16:20:38 +0200

    feat: Add "About Us" section with content localization in English and Hungarian

--- resources/views/components/pages/rolunk.blade.php
+++ resources/views/components/pages/rolunk.blade.php
@@ -5,25 +5,18 @@
         <div class="absolute inset-0 z-1 bg-gradient-to-b from-white/90 to-white/70"></div>
         <div class="relative z-10 container mx-auto space-y-3 pt-24 pb-20">
             <h2 class="mt-4 mb-16 font-bold text-5xl text-center drop-shadow text-logogray/80">
-                Rólunk</h2>
+                {{ __('About Us') }}</h2>
 
             <div class="max-w-screen-xl mx-auto p-8 backdrop-blur-3xl rounded-xl border border-white/15 shadow-xl">
                 <div class="p-6 border-b border-gray-300 hover:brightness-95 transition-all duration-300 ease-in-out">
                     <p class="mb-3 text-gray-600">
-                        Cégünket, miként a nevünk is tükrözi, azzal a szándékkal hoztuk létre, hogy valódi megoldásokkal
-                        szolgáljunk az ingatlanpiacon olyan cégek számára, akik gyors és professzionális
-                        szolgáltatásokat igényelnek ezen a területen. Továbbá azon ügyfelek részére, akik nem akarnak
-                        irodaházak keresésére és a kontakt személyek elérésére órákat szánni, csak egy mailben vagy
-                        telefonon átküldik az igényeiket és 24 órán belül ajánlatot kaphatnak a teljes budapesti „A”
-                        kategóriás irodapiaci kínálat megszűrésével az ügyfél által megadott paraméterekre fókuszálva.
+                        {{ __('About us page content 1') }}
                     </p>
                     <p class="mb-3 text-gray-600">
-                        A tulajdonosi kör 20 éve sikeresen munkálkodik az ingatlan szakmában és ez alkalommal egy olyan
-                        csapatot alapított, akik tapasztalatukkal és hozzáértésükkel készen állnak hatékonyan
-                        kiszolgálni a hozzájuk fordulókat.
+                        {{ __('About us page content 2') }}
                     </p>
                     <p class="mb-3 text-gray-600">
-                        Köszönjük, hogy megtisztel minket bizalmával!
+                        {{ __('About us page content 3') }}
                     </p>
                 </div>
             </div>
