125a7200b5bc8164e730cb09fef1c1cee4f23aa3
Cegem-360 <144362694+Cegem-360@users.noreply.github.com>
2025-06-27 16:31:48 +0200

    mobile nav layout

--- resources/views/components/layouts/navigation/hero.blade.php
+++ resources/views/components/layouts/navigation/hero.blade.php
@@ -4,7 +4,7 @@
         class="flex flex-col sm:flex-row justify-start items-center gap-8 sm:gap-16 lg:gap-32 mx-auto sm:pl-16 py-8 sm:py-16 max-w-screen-xl">
         <div class="absolute -left-56 -top-16 z-10 w-[40%] text-primary/15"><x-svg.psg-irodahazak-symbol />
         </div>
-        <div class="logo">
+        <div class="logo pt-8 sm:pt-0">
             <a href="{{ localized_route('home') }}" class="flex items-center">
                 <img src="{{ Vite::asset('resources/images/psg-irodahazak-logo.png') }}" class="mr-3 h-12 sm:h-24"
                     alt="PSG Irodaházak logo" />
--- resources/views/components/layouts/navigation/navbar.blade.php
+++ resources/views/components/layouts/navigation/navbar.blade.php
@@ -1,14 +1,14 @@
 <nav
-    class="fixed lg:absolute lg:-top-0 lg:left-[50%] lg:-translate-x-[50%] lg:-translate-y-[50%] z-50 bg-white lg:bg-transparent px-4 lg:px-0 py-2.5">
-    <div class="flex flex-wrap justify-center items-center mx-auto">
+    class="fixed lg:absolute top-11 lg:-top-0 lg:left-[50%] lg:-translate-x-[50%] lg:-translate-y-[50%] z-50 bg-white/40 backdrop-blur-[60px] lg:backdrop-blur-[0px] lg:bg-transparent px-4 lg:px-0 py-2.5">
+    <div class="flex flex-wrap justify-start items-center mx-auto">
         <div class="flex items-center lg:order-2 space-x-3">
             <!-- Mobile language switcher -->
-            <div class="lg:hidden">
+            {{-- <div class="lg:hidden">
                 <x-language-switcher />
-            </div>
+            </div> --}}
 
             <button data-collapse-toggle="mobile-menu-2" type="button"
-                class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+                class="inline-flex items-center p-2 ml-1 text-sm text-gray-800 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
                 aria-controls="mobile-menu-2" aria-expanded="false">
                 <span class="sr-only">Menü</span>
                 <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -25,7 +25,7 @@
         </div>
         <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
             <ul
-                class="flex flex-col mt-4 font-bold lg:flex-row lg:mt-0 text-white text-xl text-nowrap bg-gradient-to-b from-black/30 to-black/5 border border-black/15 backdrop-blur-3xl shadow-lg rounded-md overflow-hidden">
+                class="flex flex-col mt-4 font-bold lg:flex-row lg:mt-0 text-white text-xl text-nowrap bg-gray-400 lg:bg-transparent lg:bg-gradient-to-b lg:from-black/30 lg:to-black/5 border border-black/15 backdrop-blur-3xl shadow-lg rounded-md overflow-hidden">
                 <li>
                     <a href="{{ localized_route('home') }}"
                         class="{{ request()->routeIs(['home', 'en.home']) ? 'active' : '' }} block py-4 px-8 hover:bg-primary/80 drop-shadow duration-1000 transition-color ease-[cubic-bezier(0.19,1,0.22,1)]"
