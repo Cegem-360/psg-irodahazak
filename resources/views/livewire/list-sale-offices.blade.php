70fc62687a22bfc7d8c153e188f382ae56d182b3
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2025-07-02 16:11:33 +0200

    fix: Update route for office cards to use the correct localized route for better navigation

--- resources/views/livewire/list-sale-offices.blade.php
+++ resources/views/livewire/list-sale-offices.blade.php
@@ -60,7 +60,9 @@
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
                     @foreach ($offices ?? [] as $office)
                         <livewire:ingatlan-card :property="$office" :image="$office->getFirstImageUrl('800x600')" :title="$office->title" :title="$office->title"
-                            :description="$office->getAddressFormatedForSale()" :link="localized_route('elado-irodahazak', ['property' => $office->slug])" :key="$office->id" :small="true" />
+                            :description="$office->getAddressFormatedForSale()" :link="localized_route('properties.show-for-sale', [
+                                'property' => $office->slug,
+                            ])" :key="$office->id" :small="true" />
                     @endforeach
                 </div>
             </div>
