5b11cb8f95b4a29557e4011f4eb463d1524bac2f
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2025-07-30 16:37:29 +0200

    feat: Integrate Google Tag Manager with cookie consent handling

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ use Filament\Forms\Components\Toggle;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
+use GoogleTagManager;
 use Illuminate\Support\ServiceProvider;
+use Whitecube\LaravelCookieConsent\Facades\Cookies;
 
 final class AppServiceProvider extends ServiceProvider
 {
@@ -36,6 +38,17 @@ final class AppServiceProvider extends ServiceProvider
         // Set locale from session
         if (session()->has('locale')) {
             app()->setLocale(session('locale'));
+        }
+
+        GoogleTagManager::set('config', env('GOOGLE_TAG_ID', ''));
+        if (Cookies::hasConsentFor('ad_storage')) {
+            GoogleTagManager::set('ad_storage', 'granted');
+        }
+        if (Cookies::hasConsentFor('ad_user_data')) {
+            GoogleTagManager::set('ad_user_data', 'granted');
+        }
+        if (Cookies::hasConsentFor('ad_personalization')) {
+            GoogleTagManager::set('ad_personalization', 'granted');
         }
 
         TextColumn::configureUsing(function (TextColumn $column): void {
