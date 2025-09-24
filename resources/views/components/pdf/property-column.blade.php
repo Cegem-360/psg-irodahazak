5ec535682d0b6510b0367f09ba7e353ed928d137
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2025-09-24 12:21:30 +0200

    fix: Update property-column view to use unescaped output for second_span

--- resources/views/components/pdf/property-column.blade.php
+++ resources/views/components/pdf/property-column.blade.php
@@ -2,5 +2,5 @@
 
 <div class="flex justify-between items-center py-1.5 border-b border-gray-200">
     <span class="font-bold text-gray-600">{{ __($first_span) }}:</span>
-    <span class="font-medium text-gray-900">{{ $second_span }}</span>
+    <span class="font-medium text-gray-900">{!! $second_span !!}</span>
 </div>
