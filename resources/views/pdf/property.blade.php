b0e78581f8ea5dabd0afe322652944ccf865810f
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2025-10-30 15:10:43 +0100

    fix: Update number formatting for minimum rental price to include two decimal places

--- resources/views/pdf/property.blade.php
+++ resources/views/pdf/property.blade.php
@@ -107,7 +107,7 @@
                     @endif
 
                     @if ($property->min_kiado)
-                        <x-pdf.property-column :first_span="__('Min. Available')" :second_span="number_format((int) $property->min_kiado, 0, ',', ' ') .
+                        <x-pdf.property-column :first_span="__('Min. Available')" :second_span="number_format((int) $property->min_kiado, 2, ',', ' ') .
                             ' ' .
                             __($property->min_kiado_addons ?? '')" />
                     @endif
